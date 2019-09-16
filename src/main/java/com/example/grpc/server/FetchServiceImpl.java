6f365324c35ccd30c599b2be5d3e48124382a05b
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-16 12:19:33 +0530

    updated logs

--- src/main/java/com/example/grpc/server/FetchServiceImpl.java
+++ src/main/java/com/example/grpc/server/FetchServiceImpl.java
@@ -28,19 +28,22 @@ public class FetchServiceImpl extends FetchserviceImplBase {
 	public void fetch(FindProvision request, StreamObserver<GetPage> response) {
 
 		Date date = new Date(request.getTimestamp());
+		System.out.println("Request Payload/n"+"Date : "+request.getTimestamp());
+		System.out.println("Requested time/n"+new Date());
 		Pageable listAll = null;
 		listAll = PageRequest.of(request.getPage(), request.getSize(), Sort.by("codigo_sap_expediente").ascending());
 		Page<Provision> pg = db.findByPeriodo(date, listAll);
-		System.out.println("Inside Response-breakpoint page response" + pg);
+		//System.out.println("Inside Response-breakpoint page response" + pg);
 		int totalelements = (int) pg.getTotalElements();
-		System.out.println("Inside Response-breakpoint total elements" + totalelements);
+		//System.out.println("Inside Response-breakpoint total elements" + totalelements);
 		List<Provision> list = pg.getContent();
 
 		GetPage.Builder build = GetPage.newBuilder();
-
+		System.out.println("Respone ");
+		System.out.println("codigo_sap_expediente values with cod_sociedad");
 		for (int i = 0; i < list.size(); i++) {
-			System.out.println("printing codigo");
-			System.out.println(list.get(i).getKey().getCodigo());
+			
+			System.out.println(list.get(i).getKey().getCodigo()+"   "+list.get(i).getKey().getCod_sociedad());
 
 			build.addResult(GetResults.newBuilder().setCodigo(list.get(i).getKey().getCodigo())
 					.setCodSocieded(list.get(i).getKey().getCod_sociedad()).setTimestamp(list.get(i).getTimestamp())
